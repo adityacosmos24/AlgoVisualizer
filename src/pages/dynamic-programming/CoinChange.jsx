a2d5040955e187c785a1407ca3504c1aa8d1b6b4
Aditya Agrawal <adityacosmos24@gmail.com>
2026-01-07 00:19:26 +0530

    fixed case sensitive issues

--- src/pages/dynamic-programming/CoinChange.jsx
+++ src/pages/dynamic-programming/CoinChange.jsx
@@ -1,6 +1,6 @@
 // src/pages/dynamic-programming/CoinChange.jsx
 import React from "react";
-import CoinChangeVisualizer from "../../components/dynamic-programming/CoinChangeVisualizer";
+import CoinChangeVisualizer from "../../components/dynamic-programming/coinChangeVisualizer";
 
 export default function CoinChange() {
     return <CoinChangeVisualizer />;
